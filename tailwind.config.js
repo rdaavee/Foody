a57e9e75b1e8e81d76a7a0bc8c0d9e41874146f8
rdaavee <rdavee0412@gmail.com>
2024-11-16 00:32:22 +0800

    add custom color palette and RedHatText font to Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        appGreen: "hsl(159, 69%, 38%)",
+        appRed: "hsl(14, 86%, 42%)",
+        appRose: {
+          50: "hsl(20, 50%, 98%)",
+          100: "hsl(13, 31%, 94%)",
+          300: "hsl(14, 25%, 72%)",
+          400: "hsl(7, 20%, 60%)",
+          500: "hsl(12, 20%, 44%)",
+          900: "hsl(14, 65%, 9%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
